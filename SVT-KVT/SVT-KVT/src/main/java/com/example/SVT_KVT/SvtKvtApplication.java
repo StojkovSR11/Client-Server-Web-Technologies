325b44c34dcb07ebf40c9b958a3be7d017342d1d
StojkovSR11 <stojkov.sr11.2022@uns.ac.rs>
2024-08-23 10:36:31 +0200

    Inicijalizacija Aplikacija

---
+++ SVT-KVT/SVT-KVT/src/main/java/com/example/SVT_KVT/SvtKvtApplication.java
@@ -0,0 +1,14 @@
+package com.example.SVT_KVT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+public class SvtKvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvtKvtApplication.class, args);
+	}
+
+}
---
+++ SVT-KVT/SVT-KVT/src/test/java/com/example/SVT_KVT/SvtKvtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SVT_KVT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvtKvtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
