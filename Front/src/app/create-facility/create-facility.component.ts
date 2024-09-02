1acad1355079ffd057b7f619eb339b00cbfc01f2
StojkovSR11 <stojkov.sr11.2022@uns.ac.rs>
2024-09-02 23:15:44 +0200

    Izmena forme

--- Front/src/app/create-facility/create-facility.component.ts
+++ Front/src/app/create-facility/create-facility.component.ts
@@ -1,33 +1,49 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FacilityService } from '../facilities/facility.service';
 import { Facility } from '../facilities/facility.model';
 
 @Component({
   selector: 'app-create-facility',
   templateUrl: './create-facility.component.html',
-  styleUrls: ['./create-facility.component.css']
+  styleUrls: ['./create-facility.component.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule]  // Import ReactiveFormsModule here
 })
 export class CreateFacilityComponent {
-  facility: Facility = {
-    id: 0,  // Set default or handle auto-increment
-    name: '',
-    description: '',
-    createdAt: new Date().toISOString(),  // Set the current date
-    address: '',
-    city: '',
-    totalRating: 0,
-    active: true,
-    workDays: [],
-    disciplines: []
-  };
+  facilityForm: FormGroup;
 
-  constructor(private facilityService: FacilityService, private router: Router) {}
-
-  createFacility(): void {
-    this.facilityService.createFacility(this.facility).subscribe(() => {
-      this.router.navigate(['/']);  // Redirect to the facilities list or another page
+  constructor(
+    private facilityService: FacilityService, 
+    private router: Router, 
+    private fb: FormBuilder
+  ) {
+    this.facilityForm = this.fb.group({
+      name: ['', Validators.required],
+      description: [''],
+      createdAt: [new Date().toISOString()],
+      address: ['', Validators.required],
+      city: ['', Validators.required],
+      workDays: [[]],
+      disciplines: [[]]
     });
   }
+
+  createFacility(): void {
+    if (this.facilityForm.valid) {
+      const facility: Facility = {
+        ...this.facilityForm.value,
+        totalRating: 0,  // Set default value
+        active: true     // Set default value
+      };
+      this.facilityService.createFacility(facility).subscribe(() => {
+        this.router.navigate(['/']);  // Redirect to the facilities list or another page
+      });
+    }
+  }
 }
+
+
+
 
